d0acc5602cb5fb0c66142f8c6319bce8836ca327
Thiago Gomes de Oliveira <infogomes@gmail.com>
2022-08-12 15:21:36 +0100

    User microservice created

---
+++ finan-contr-user/src/main/java/br/com/infogomes/financontruser/FinanContrUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.infogomes.financontruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanContrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanContrUserApplication.class, args);
+	}
+
+}
---
+++ finan-contr-user/src/test/java/br/com/infogomes/financontruser/FinanContrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infogomes.financontruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanContrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
