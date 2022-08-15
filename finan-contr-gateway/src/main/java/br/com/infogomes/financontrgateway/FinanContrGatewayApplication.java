634535e8b68b0ddf59410936544a68bf2a025193
Thiago Gomes de Oliveira <infogomes@gmail.com>
2022-08-15 16:23:08 +0100

    Gateway project created

---
+++ finan-contr-gateway/src/main/java/br/com/infogomes/financontrgateway/FinanContrGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.infogomes.financontrgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanContrGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanContrGatewayApplication.class, args);
+	}
+
+}
---
+++ finan-contr-gateway/src/test/java/br/com/infogomes/financontrgateway/FinanContrGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infogomes.financontrgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanContrGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
