e177a40c909f61e85dcb1ee087a27cb5331b5403
Thiago Gomes de Oliveira <infogomes@gmail.com>
2022-08-15 15:31:39 +0100

    Eureka project created

---
+++ finan-contr-eureka/src/main/java/br/com/infogomes/financontreureka/FinanContrEurekaApplication.java
@@ -0,0 +1,13 @@
+package br.com.infogomes.financontreureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanContrEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanContrEurekaApplication.class, args);
+	}
+
+}
